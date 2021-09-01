34f29774bdfdbd2c6f395fe3c952b41494bdb26f
Diego <diegoromerogalvan925@gmail.com>
2021-08-31 22:15:45 -0500

    Agregados slashs commands

--- src/Commands/devs/shupdate.ts
+++ src/Commands/devs/shupdate.ts
@@ -36,7 +36,7 @@ async run(base: TempContext) {
         
           base.reply(`El escaneo de / comandos se ha llevado satisfactoriamente y se han subido 5 comandos en total al servidor.\n\n**https://discord.com/oauth2/authorize?client_id=${base.client.user.id}&scope=applications.commands** Aqui tienes el link para que los actulices bb hermoso precioso`)
         } catch (error) {
-          base.reply(`He we, no pude subir los comandos esos, porq\n\n\`${error}\``)
+          base.reply(`No se pueden subir los comandos a la API debido a que ha pasado un error de mientras que intentaba hacerlo, el codigo de error es:\n\n\`${error}\``)
         }
       })();
 
