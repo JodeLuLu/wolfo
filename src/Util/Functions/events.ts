fe30f5680187e5f2ed478da1a04a8169d8969b84
Diego <diegoromerogalvan925@gmail.com>
2021-09-01 22:57:01 -0500

    Creado interactionUtil.ts

--- src/Util/Functions/events.ts
+++ src/Util/Functions/events.ts
@@ -1,32 +1,60 @@
-import { Collection, Message, MessageEmbed, PartialMessage, RateLimitData, Snowflake } from "discord.js";
+import {
+  Collection,
+  Message,
+  MessageEmbed,
+  PartialMessage,
+  RateLimitData,
+  Snowflake,
+} from "discord.js";
 import { Client } from "discord.js";
 import { Apodo, Members, Messages, Roles } from "../Classes/logger";
 import { NewMember } from "../Classes/MemberUtil";
-import { SlashCommands } from "../Classes/slashCommands";
 import { TimeStamp } from "../Classes/time";
 import { RateLimited, richPresence } from "./clientUtil";
+import { interactionUtil } from "./interactionUtil";
 import { messageUtil } from "./messageUtil";
-import { verificaction } from "./verification";
 
 export function eventsCentral(client: Client) {
-
-    try {
+  try {
     // WolfyBot
-    client.on("ready", () => {richPresence(client)});
+    client.on("ready", () => {
+      richPresence(client);
+    });
 
-    client.on("rateLimit", (ratelimit: RateLimitData) => {RateLimited(ratelimit, client)});
-    
+    client.on("rateLimit", (ratelimit: RateLimitData) => {
+      RateLimited(ratelimit, client);
+    });
+
     // Mensajes
-    client.on("messageCreate", (message) => {messageUtil(message)})
-    client.on("messageDelete", async (message: Message) => {if (message.partial) await message.fetch(); const a = new Messages(message); a.deleted()});
-    client.on("messageUpdate", (viejo: Message, nuevo: Message) => {new Messages(nuevo, viejo).edited()});
-    client.on(`messageDeleteBulk`, (messages: Collection<Snowflake, Message>) => {new Messages(null, null, null, messages).BulkDelete()})
-    client.on(`interactionCreate`, (interaction) => {verificaction(interaction); new SlashCommands(client, interaction).SlashUtil()})
+    client.on("messageCreate", (message) => {
+      messageUtil(message);
+    });
+    client.on("messageDelete", async (message: Message) => {
+      if (message.partial) await message.fetch();
+      const a = new Messages(message);
+      a.deleted();
+    });
+    client.on("messageUpdate", (viejo: Message, nuevo: Message) => {
+      new Messages(nuevo, viejo).edited();
+    });
+    client.on(
+      `messageDeleteBulk`,
+      (messages: Collection<Snowflake, Message>) => {
+        new Messages(null, null, null, messages).BulkDelete();
+      }
+    );
+    client.on(`interactionCreate`, (interaction) => {
+      interactionUtil(interaction);
+    });
 
     // Member
-    client.on("guildMemberUpdate", (viejo, nuevo) => {new Roles(nuevo, viejo).quitado(); new Roles(nuevo, viejo).puesto(); new Apodo(viejo, nuevo).cambiado();});
-    client.on("guildMemberAdd", (miembro) => {const a = new NewMember(miembro);});
-    
-
-    } catch (e) {}
+    client.on("guildMemberUpdate", (viejo, nuevo) => {
+      new Roles(nuevo, viejo).quitado();
+      new Roles(nuevo, viejo).puesto();
+      new Apodo(viejo, nuevo).cambiado();
+    });
+    client.on("guildMemberAdd", (miembro) => {
+      const a = new NewMember(miembro);
+    });
+  } catch (e) {}
 }
