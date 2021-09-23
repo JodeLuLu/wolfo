4f9c98653be6a5dfc92152719a267b4d87dcb158
Cheree <71246795+Chere3@users.noreply.github.com>
2021-09-23 14:20:51 -0500

    Actualizado todo.

--- src/Util/assets/limitedSlashCommand.ts
+++ src/Util/assets/limitedSlashCommand.ts
@@ -1,45 +1,48 @@
 export const limitedCommand = [
-    {
-        "name": "roled",
-        "description": "Agrega/remueve roles especiales del usuario.",
-        "options": [
-            {
-                "name": "nsfw",
-                "description": "Agrega/quita acceso a los canales nsfw.",
-                "type": 1
-            },
-            {
-                "name": "actualizaciones",
-                "description": "Recibe/remueve los pings de los canales de servidor.",
-                "type": 1
-            },
-            {
-                "name": "noticias",
-                "description": "Recibe/remueve los pings de los canales de noticias de Wolfy.",
-                "type": 1
-            },
-            {
-                "name": "eventos",
-                "description": "Recibe/remueve los pings de los canales de eventos del servidor.",
-                "type": 1
-            },
-            {
-                "name": "alianzas",
-                "description": "Recibe/remueve los pings de los canales de alianzas del servidor.",
-                "type": 1
-            }
-        ]
-    },
-    {
-        "name": "coded",
-        "description": "Calcula una expresion con este comando.",
-        "options": [
-            {
-                "name": "codigo",
-                "description": "El codigo a evaluar.",
-                "type": 3,
-                "required": true
-            }
-        ]
-    }    
-]
+  {
+    name: "role",
+    description: "Agrega/remueve roles especiales del usuario.",
+    options: [
+      {
+        name: "nsfw",
+        description: "Agrega/quita acceso a los canales nsfw.",
+        type: 1,
+      },
+      {
+        name: "actualizaciones",
+        description: "Recibe/remueve los pings de los canales de servidor.",
+        type: 1,
+      },
+      {
+        name: "noticias",
+        description:
+          "Recibe/remueve los pings de los canales de noticias de Wolfy.",
+        type: 1,
+      },
+      {
+        name: "eventos",
+        description:
+          "Recibe/remueve los pings de los canales de eventos del servidor.",
+        type: 1,
+      },
+      {
+        name: "alianzas",
+        description:
+          "Recibe/remueve los pings de los canales de alianzas del servidor.",
+        type: 1,
+      },
+    ],
+  },
+  {
+    name: "code",
+    description: "Calcula una expresion con este comando.",
+    options: [
+      {
+        name: "codigo",
+        description: "El codigo a evaluar.",
+        type: 3,
+        required: true,
+      },
+    ],
+  },
+];
